21254dc29648375662a5d3c289b4c0c1040d1f4e
ricnef2121 <34247337+ricnef2121@users.noreply.github.com>
2023-12-04 01:28:21 -0600

    feat: add envrionmente variables
    and scripts in package.json

---
+++ app.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const Item = require("./Item");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_CONNECTION, {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("connected");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create
+app.post("/items", async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    const savedItem = await newItem.save();
+    res.json(savedItem);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Read
+app.get("/items", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update
+app.put("/items/:id", async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedItem);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Delete
+app.delete("/items/:id", async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndDelete(req.params.id);
+    res.json(deletedItem);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
