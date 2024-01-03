4a75d91964b9392b0f8f8681cc1a9fdf680a6c3c
ricnef2121 <ricnef2121@gmail.com>
2024-01-03 10:42:08 -0600

    feat: add ownership in terraform

--- main.tf
+++ main.tf
@@ -21,34 +21,33 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+
+
 resource "aws_s3_bucket" "new_bucket" {
   bucket = "demo-github-action-tf-todo-list"
-
   tags = {
     Environment = "Prod"
   }
 }
-resource "aws_s3_bucket_public_access_block" "todo-list" {
- bucket = aws_s3_bucket.new_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
 resource "aws_s3_object" "provision_source_files" {
-  key    = "todo_list"
+  key    = "index.html"
   bucket = aws_s3_bucket.new_bucket.id
-  source     = "dist/index.html"
+  source = "dist/index.html"
 }
 
- 
+resource "aws_s3_bucket_ownership_controls" "new_bucket" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
 
 variable "TFC_AWS_ACCESS_KEY_ID" {
   type    = string
   default = ""
-
 }
 variable "TFC_AWS_SECRET_ACCESS_KEY" {
   type    = string
