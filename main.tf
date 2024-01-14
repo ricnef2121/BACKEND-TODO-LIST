f2ad3edd4de4c79905d203760de825ac9d945cfb
ricnef2121 <ricnef2121@gmail.com>
2024-01-14 01:29:00 -0600

    feat:add region

--- main.tf
+++ main.tf
@@ -4,13 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-  }
-  backend "s3" {
-    key = "aws/ec2-deploy/terraform.tfstate"
-  }
+  } 
 }
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 resource "aws_instance" "servernode" {
