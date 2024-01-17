2b695a3d45ba55936cc000918580cef1b0bf0d20
ricnef2121 <ricnef2121@gmail.com>
2024-01-17 00:50:38 -0600

    feat: add new configuration to create ec2 instance with bi/bash, add new configuration to pushy docker imagen

--- terra-third-time/main.tf
+++ terra-third-time/main.tf
@@ -79,8 +79,7 @@ resource "null_resource" "docker_packaging" {
   provisioner "local-exec" {
     command = <<EOF
 	    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com
-	    gradle build -p backend
-	    docker build -t "${aws_ecr_repository.backend.repository_url}:latest" -f ./Dockerfile .
+	    docker build -t "${aws_ecr_repository.backend.repository_url}:latest" -f backend/Dockerfile .
 	    docker push "${aws_ecr_repository.backend.repository_url}:latest"
 	    EOF
   }
