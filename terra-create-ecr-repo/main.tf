bd87b77d08e0c18dda325028c25606b35e350d64
ricnef2121 <ricnef2121@gmail.com>
2024-01-17 17:46:00 -0600

    feat: add configuration to push o docker file with github actions

--- terra-create-ecr-repo/main.tf
+++ terra-create-ecr-repo/main.tf
@@ -16,6 +16,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "backend"
   image_tag_mutability = "MUTABLE"
+  force_delete = false
 
   image_scanning_configuration {
     scan_on_push = true
