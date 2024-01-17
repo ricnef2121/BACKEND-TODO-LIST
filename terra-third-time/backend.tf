c6120d0d3fae9989d3117d92b837919098bd975b
ricnef2121 <ricnef2121@gmail.com>
2024-01-17 00:31:31 -0600

    feat: add new configuration to create ec2 instance with bi/bash, add new configuration to pushy docker imagen

--- terra-third-time/backend.tf
+++ terra-third-time/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  # required_providers {
-  #   docker = {
-  #     source  = "kreuzwerker/docker"
-  #     version = "3.0.2"
-  #   }
-  # }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
 
   backend "remote" {
     organization = "example-org-35309e"
@@ -13,8 +13,4 @@ terraform {
       name = "learn-terraform-github-actions"
     }
   }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
 }
--- terra-third-time/providers.tf
+++ terra-third-time/providers.tf
@@ -4,10 +4,16 @@ provider "aws" {
   region     = var.region
 }
 
+
 provider "docker" {
-  registry_auth {
-    address  = local.aws_ecr_url
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
+  host = "unix:///var/run/docker.sock"
 }
+
+
+# provider "docker" {
+#   registry_auth {
+#     address  = local.aws_ecr_url
+#     username = data.aws_ecr_authorization_token.token.user_name
+#     password = data.aws_ecr_authorization_token.token.password
+#   }
+# }
